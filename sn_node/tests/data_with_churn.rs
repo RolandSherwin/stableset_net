c419470b616ef4e22f25fa6b5628418281b89002
RolandSherwin <RolandSherwin@protonmail.com>
2023-07-13 15:16:32 +0530

    --wip-- [skip ci]

--- sn_node/tests/data_with_churn.rs
+++ sn_node/tests/data_with_churn.rs
@@ -6,53 +6,56 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
-use safenode_proto::{safe_node_client::SafeNodeClient, NodeInfoRequest, RestartRequest};
+mod common;
 
 use bytes::Bytes;
+use common::{
+    get_client, node_restart,
+    safenode_proto::{safe_node_client::SafeNodeClient, NodeInfoRequest, RecordAddressesRequest},
+};
 use eyre::{bail, eyre, Result};
+use itertools::Itertools;
+use libp2p::{
+    kad::{KBucketKey, RecordKey},
+    PeerId,
+};
 use rand::{rngs::OsRng, Rng};
 use sn_client::{Client, Error, Files};
 use sn_logging::{init_logging, LogFormat, LogOutputDest};
-use sn_peers_acquisition::parse_peer_addr;
+use sn_networking::{sort_peers_by_key, CLOSE_GROUP_SIZE};
 use sn_protocol::{
     storage::{ChunkAddress, RegisterAddress},
     NetworkAddress,
 };
 use std::{
-    collections::{BTreeMap, VecDeque},
+    collections::{BTreeMap, HashMap, HashSet, VecDeque},
     fmt,
     net::{IpAddr, Ipv4Addr, SocketAddr},
-    path::Path,
-    sync::Arc,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
     time::{Duration, Instant},
 };
-use tokio::{fs::remove_dir_all, sync::RwLock, time::sleep};
+use tokio::{sync::RwLock, time::sleep};
 use tonic::Request;
 use tracing::trace;
 use tracing_core::Level;
 use xor_name::XorName;
 
-// this includes code generated from .proto files
-#[allow(unused_qualifications, unreachable_pub, clippy::unwrap_used)]
-mod safenode_proto {
-    tonic::include_proto!("safenode_proto");
-}
-
 const NODE_COUNT: u32 = 25;
-
-const EXTRA_CHURN_COUNT: u32 = 5;
-const CHURN_CYCLES: u32 = 1;
-const CHUNK_CREATION_RATIO_TO_CHURN: u32 = 5;
-const REGISTER_CREATION_RATIO_TO_CHURN: u32 = 5;
-
 const CHUNKS_SIZE: usize = 1024 * 1024;
-
-const CONTENT_QUERY_RATIO_TO_CHURN: u32 = 12;
 const MAX_NUM_OF_QUERY_ATTEMPTS: u8 = 5;
 
 // Default total amount of time we run the checks for before reporting the outcome.
 // It can be overriden by setting the 'TEST_DURATION_MINS' env var.
 const TEST_DURATION: Duration = Duration::from_secs(60 * 60); // 1hr
+
+// Churn interval is set based on the dead peer detection interval
+// This is to make sure that the we can accurately verify the data location post churn
+const CHURN_INTERVAL: Duration = Duration::from_secs(5 * 60); // 5 mins
+const PUT_DELAY: Duration = Duration::from_secs(5);
+const GET_DELAY: Duration = Duration::from_secs(3);
 
 type ContentList = Arc<RwLock<VecDeque<NetworkAddress>>>;
 
@@ -82,15 +85,11 @@ async fn data_availability_during_churn() -> Result<()> {
         TEST_DURATION
     };
 
-    let churn_period = if let Ok(str) = std::env::var("TEST_CHURN_CYCLES") {
-        let cycles = str.parse::<u32>()?;
-        test_duration / (cycles * NODE_COUNT)
-    } else {
-        // Ensure at least some nodes got churned twice.
-        test_duration / std::cmp::max(CHURN_CYCLES * NODE_COUNT, NODE_COUNT + EXTRA_CHURN_COUNT)
-    };
+    if TEST_DURATION < CHURN_INTERVAL {
+        bail!("The total test duration {TEST_DURATION:?} should be greater than the churn interval {CHURN_INTERVAL:?}");
+    }
 
-    println!("Nodes will churn every {:?}", churn_period);
+    println!("Nodes will churn every {CHURN_INTERVAL:?}");
 
     // Create a cross thread usize for tracking churned nodes
     let churn_count = Arc::new(RwLock::new(0_usize));
@@ -123,17 +122,23 @@ async fn data_availability_during_churn() -> Result<()> {
 
     // Shared bucket where we keep track of content created/stored on the network
     let content = ContentList::default();
+    let verification_failed = Arc::new(AtomicBool::new(false));
 
     // Upload some chunks before carry out any churning.
 
     // Spawn a task to store Chunks at random locations, at a higher frequency than the churning events
-    store_chunks_task(client.clone(), content.clone(), churn_period);
+    store_chunks_task(client.clone(), content.clone());
 
-    // Wait one churn period _before_ we start churning, to get some data PUT on the network
-    sleep(churn_period).await;
+    // Wait for sometime _before_ we start churning, to get some data PUT on the network
+    sleep(5 * PUT_DELAY).await;
 
     // Spawn a task to churn nodes
-    churn_nodes_task(churn_count.clone(), test_duration, churn_period);
+    churn_nodes_task(
+        churn_count.clone(),
+        content.clone(),
+        test_duration,
+        verification_failed.clone(),
+    );
 
     // Shared bucket where we keep track of the content which erred when creating/storing/fetching.
     // We remove them from this bucket if we are then able to query/fetch them successfully.
@@ -145,25 +150,15 @@ async fn data_availability_during_churn() -> Result<()> {
 
     // Spawn a task to create Registers at random locations, at a higher frequency than the churning events
     if !chunks_only {
-        create_registers_task(client.clone(), content.clone(), churn_period);
+        create_registers_task(client.clone(), content.clone());
     }
 
     // Spawn a task to randomly query/fetch the content we create/store
-    query_content_task(
-        client.clone(),
-        content.clone(),
-        content_erred.clone(),
-        churn_period,
-    );
+    query_content_task(client.clone(), content.clone(), content_erred.clone());
 
     // Spawn a task to retry querying the content that failed, up to 'MAX_NUM_OF_QUERY_ATTEMPTS' times,
     // and mark them as failures if they effectivelly cannot be retrieved.
-    retry_query_content_task(
-        client.clone(),
-        content_erred.clone(),
-        failures.clone(),
-        churn_period,
-    );
+    retry_query_content_task(client.clone(), content_erred.clone(), failures.clone());
 
     let start_time = Instant::now();
     while start_time.elapsed() < test_duration {
@@ -174,7 +169,7 @@ async fn data_availability_during_churn() -> Result<()> {
             failed.len(),
             failed.values()
         );
-        sleep(churn_period).await;
+        sleep(PUT_DELAY * 2).await;
     }
 
     println!();
@@ -203,7 +198,7 @@ async fn data_availability_during_churn() -> Result<()> {
     let content = content.read().await;
     let uploaded_content_count = content.len();
     for net_addr in content.iter() {
-        let result = final_retry_query_content(&client, net_addr, churn_period).await;
+        let result = final_retry_query_content(&client, net_addr).await;
         assert!(
             result.is_ok(),
             "Failed to query content at {net_addr:?} with error {result:?}"
@@ -219,6 +214,11 @@ async fn data_availability_during_churn() -> Result<()> {
         "Not all content was queried"
     );
 
+    // check if verification failed
+    if verification_failed.load(Ordering::SeqCst) {
+        bail!("Verificaion failed");
+    }
+
     drop(log_appender_guard);
 
     println!("Test passed after running for {:?}.", start_time.elapsed());
@@ -226,24 +226,19 @@ async fn data_availability_during_churn() -> Result<()> {
 }
 
 // Spawns a task which periodically creates Registers at random locations.
-fn create_registers_task(client: Client, content: ContentList, churn_period: Duration) {
+fn create_registers_task(client: Client, content: ContentList) {
     let _handle = tokio::spawn(async move {
-        // Create Registers at a higher frequency than the churning events
-        let delay = churn_period / REGISTER_CREATION_RATIO_TO_CHURN;
-
         loop {
             let xorname = XorName(rand::random());
             let tag = rand::random();
 
             let addr = RegisterAddress { name: xorname, tag };
-            println!("Creating Register at {addr:?} in {delay:?}");
-            sleep(delay).await;
+            println!("Creating Register at {addr:?} in {PUT_DELAY:?}");
+            sleep(PUT_DELAY).await;
 
+            let mut content_write_lock = content.write().await;
             match client.create_register(xorname, tag).await {
-                Ok(_) => content
-                    .write()
-                    .await
-                    .push_back(NetworkAddress::RegisterAddress(addr)),
+                Ok(_) => content_write_lock.push_back(NetworkAddress::RegisterAddress(addr)),
                 Err(err) => println!("Discarding new Register ({addr:?}) due to error: {err:?}"),
             }
         }
@@ -251,11 +246,8 @@ fn create_registers_task(client: Client, content: ContentList, churn_period: Dur
 }
 
 // Spawns a task which periodically stores Chunks at random locations.
-fn store_chunks_task(client: Client, content: ContentList, churn_period: Duration) {
+fn store_chunks_task(client: Client, content: ContentList) {
     let _handle = tokio::spawn(async move {
-        // Store Chunks at a higher frequency than the churning events
-        let delay = churn_period / CHUNK_CREATION_RATIO_TO_CHURN;
-
         let file_api = Files::new(client);
         let mut rng = OsRng;
         loop {
@@ -270,17 +262,15 @@ fn store_chunks_task(client: Client, content: ContentList, churn_period: Duratio
                     .calculate_address(bytes.clone())
                     .expect("Failed to calculate new Chunk address"),
             );
-            println!("Storing Chunk at {addr:?} in {delay:?}");
-            sleep(delay).await;
+            println!("Storing Chunk at {addr:?} in {PUT_DELAY:?}");
+            sleep(PUT_DELAY).await;
 
+            let mut content_write_lock = content.write().await;
             match file_api
                 .upload_with_proof(bytes, &BTreeMap::default())
                 .await
             {
-                Ok(_) => content
-                    .write()
-                    .await
-                    .push_back(NetworkAddress::ChunkAddress(addr)),
+                Ok(_) => content_write_lock.push_back(NetworkAddress::ChunkAddress(addr)),
                 Err(err) => println!("Discarding new Chunk ({addr:?}) due to error: {err:?}"),
             }
         }
@@ -289,27 +279,21 @@ fn store_chunks_task(client: Client, content: ContentList, churn_period: Duratio
 
 // Spawns a task which periodically queries a content by randomly choosing it from the list
 // of content created by another task.
-fn query_content_task(
-    client: Client,
-    content: ContentList,
-    content_erred: ContentErredList,
-    churn_period: Duration,
-) {
+fn query_content_task(client: Client, content: ContentList, content_erred: ContentErredList) {
     let _handle = tokio::spawn(async move {
-        let delay = churn_period / CONTENT_QUERY_RATIO_TO_CHURN;
         loop {
             let len = content.read().await.len();
             if len == 0 {
-                println!("No content created/stored just yet, let's try in {delay:?} ...");
-                sleep(delay).await;
+                println!("No content created/stored just yet, let's try in {GET_DELAY:?} ...");
+                sleep(GET_DELAY).await;
                 continue;
             }
 
             // let's choose a random content to query, picking it from the list of created
             let index = rand::thread_rng().gen_range(0..len);
             let net_addr = content.read().await[index].clone();
-            trace!("Querying content (bucket index: {index}) at {net_addr:?} in {delay:?}");
-            sleep(delay).await;
+            trace!("Querying content (bucket index: {index}) at {net_addr:?} in {GET_DELAY:?}");
+            sleep(GET_DELAY).await;
 
             match query_content(&client, &net_addr).await {
                 Ok(_) => {
@@ -337,16 +321,17 @@ fn query_content_task(
 }
 
 // Spawns a task which periodically picks up a node, and restarts it to cause churn in the network.
+// Also verifies the location of the data on the network post churn
 fn churn_nodes_task(
     churn_count: Arc<RwLock<usize>>,
+    content: ContentList,
     test_duration: Duration,
-    churn_period: Duration,
+    verification_failed: Arc<AtomicBool>,
 ) {
     let start = Instant::now();
     let _handle = tokio::spawn(async move {
         let mut node_index = 1;
         let mut addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12000);
-        let delay = churn_period;
         loop {
             // break out if we've run the duration of churn
             if start.elapsed() > test_duration {
@@ -370,7 +355,23 @@ fn churn_nodes_task(
                 node_index = 1;
             }
 
-            sleep(delay).await;
+            sleep(CHURN_INTERVAL).await;
+            // read lock to make sure we don't put new data while verification is being performed
+            let _read_lock = content.read().await;
+            // verify data location
+            let stored_records = match get_peers_and_records().await {
+                Ok(stored_records) => stored_records,
+                Err(err) => {
+                    println!("Failed to get peers and their records addresses {err:?}");
+                    verification_failed.store(true, Ordering::SeqCst);
+                    continue;
+                }
+            };
+            if let Err(err) = verify_location(&stored_records, content.clone()).await {
+                println!("Failed to verify location {err:?}");
+                verification_failed.store(true, Ordering::SeqCst);
+                continue;
+            }
         }
     });
 }
@@ -381,10 +382,9 @@ fn retry_query_content_task(
     client: Client,
     content_erred: ContentErredList,
     failures: ContentErredList,
-    churn_period: Duration,
 ) {
     let _handle = tokio::spawn(async move {
-        let delay = 2 * churn_period;
+        let delay = 10 * GET_DELAY;
         loop {
             sleep(delay).await;
 
@@ -413,11 +413,7 @@ fn retry_query_content_task(
     });
 }
 
-async fn final_retry_query_content(
-    client: &Client,
-    net_addr: &NetworkAddress,
-    churn_period: Duration,
-) -> Result<()> {
+async fn final_retry_query_content(client: &Client, net_addr: &NetworkAddress) -> Result<()> {
     let mut attempts = 1;
     loop {
         println!("Querying content at {net_addr:?}, attempt: #{attempts} ...");
@@ -426,7 +422,7 @@ async fn final_retry_query_content(
                 bail!("Final check: Content is still not retrievable at {net_addr:?} after {attempts} attempts: {last_err:?}");
             } else {
                 attempts += 1;
-                let delay = 2 * churn_period;
+                let delay = 10 * GET_DELAY;
                 sleep(delay).await;
                 continue;
             }
@@ -437,42 +433,65 @@ async fn final_retry_query_content(
     }
 }
 
-async fn node_restart(addr: SocketAddr) -> Result<()> {
-    let endpoint = format!("https://{addr}");
-    let mut client = SafeNodeClient::connect(endpoint).await?;
+async fn get_peers_and_records() -> Result<HashMap<PeerId, HashSet<RecordKey>>> {
+    let mut result = HashMap::new();
+    for node_index in 1..NODE_COUNT + 1 {
+        println!("getting addresses for {node_index:?}");
+        let mut addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12000);
+        addr.set_port(12000 + node_index as u16);
+        let endpoint = format!("https://{addr}");
+        let mut rpc_client = SafeNodeClient::connect(endpoint).await?;
 
-    let response = client.node_info(Request::new(NodeInfoRequest {})).await?;
-    let log_dir = Path::new(&response.get_ref().log_dir);
-    let root_dir = log_dir
-        .parent()
-        .ok_or_else(|| eyre!("could not obtain parent from logging directory"))?;
+        // get the record addresses
+        let response = rpc_client
+            .record_addresses(Request::new(RecordAddressesRequest {}))
+            .await?;
+        #[allow(clippy::mutable_key_type)]
+        let keys = response
+            .get_ref()
+            .addresses
+            .iter()
+            .cloned()
+            .map(RecordKey::from)
+            .collect();
 
-    // remove Chunks records
-    let chunks_records = root_dir.join("record_store");
-    if let Ok(true) = chunks_records.try_exists() {
-        println!("Removing Chunks records from {}", chunks_records.display());
-        remove_dir_all(chunks_records).await?;
+        // get the peer_id
+        let response = rpc_client
+            .node_info(Request::new(NodeInfoRequest {}))
+            .await?;
+        let peer_id = PeerId::from_bytes(&response.get_ref().peer_id)?;
+        result.insert(peer_id, keys);
     }
+    Ok(result)
+}
 
-    // remove Registers records
-    let registers_records = root_dir.join("registers");
-    if let Ok(true) = registers_records.try_exists() {
-        println!(
-            "Removing Registers records from {}",
-            registers_records.display()
-        );
-        remove_dir_all(registers_records).await?;
+// Verfies that the chunk is stored by the actual closest peers to the RecordKey
+async fn verify_location(
+    stored_records: &HashMap<PeerId, HashSet<RecordKey>>,
+    content: ContentList,
+) -> Result<()> {
+    #[allow(clippy::mutable_key_type)]
+    let all_peers = stored_records.keys().cloned().collect_vec();
+    for addr in content.read().await.iter() {
+        let key = RecordKey::new(&addr.as_bytes());
+        println!("Verifying {addr:?}");
+        let record_key = KBucketKey::from(key.to_vec());
+        let expected_closest_peers =
+            sort_peers_by_key(all_peers.to_vec(), &record_key, CLOSE_GROUP_SIZE)?
+                .into_iter()
+                .collect::<HashSet<_>>();
+        for expected in &expected_closest_peers {
+            if !stored_records
+                .get(expected)
+                .ok_or_else(|| {
+                    eyre!("Expected peer {expected}, should be present in stored_records")
+                })?
+                .contains(&key)
+            {
+                return Err(eyre!("Record {addr:?} is not stored inside {expected:?}\nstored_records {stored_records:?}"));
+            }
+        }
     }
-
-    let _response = client
-        .restart(Request::new(RestartRequest { delay_millis: 0 }))
-        .await?;
-
-    println!(
-        "Node restart requested to RPC service at {addr}, and removed all its chunks and registers records at {}",
-        log_dir.display()
-    );
-
     Ok(())
 }
 
@@ -489,24 +508,4 @@ async fn query_content(client: &Client, net_addr: &NetworkAddress) -> Result<(),
         }
         _other => Ok(()), // we don't create/store any other type of content in this test yet
     }
-}
-
-async fn get_client() -> Client {
-    let secret_key = bls::SecretKey::random();
-
-    let bootstrap_peers = if !cfg!(feature = "local-discovery") {
-        match std::env::var("SAFE_PEERS") {
-            Ok(str) => match parse_peer_addr(&str) {
-                Ok(peer) => Some(vec![peer]),
-                Err(err) => panic!("Cann't parse SAFE_PEERS {str:?} with error {err:?}"),
-            },
-            Err(err) => panic!("Cann't get env var SAFE_PEERS with error {err:?}"),
-        }
-    } else {
-        None
-    };
-    println!("Client bootstrap with peer {bootstrap_peers:?}");
-    Client::new(secret_key, bootstrap_peers, None)
-        .await
-        .expect("Client shall be successfully created.")
 }
